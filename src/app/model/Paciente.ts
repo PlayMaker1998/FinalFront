90999932d42f4a7581412ac1934e547c7809d00a
PlayMaker1998 <38365977+PlayMaker1998@users.noreply.github.com>
2024-07-04 02:17:19 -0500

    frontend final

--- src/app/model/Paciente.ts
+++
@@ -1,9 +0,0 @@
-export interface Paciente {
-  id_boton_emergencia: number;
-  nombres: string;
-  apellidos: string;
-  telefono: string;
-  direccion: string;
-  correo: string;
-  test_puntuacion: number;
-}
